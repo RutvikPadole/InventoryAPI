a8421495a28b24b04f07b72991d6ba5f0ded4678
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-16 22:44:54 +0530

    Update IProductService file

--- src/Application/Services/IProductService.cs
+++ src/Application/Services/IProductService.cs
@@ -7,7 +7,7 @@ public interface IProductService
     {
         List<Product> GetAllProducts();
         public Product GetProductById(int id);
-        void AddProduct(Product product);
+        public void AddProduct(Product product);
     public void UpdateProduct(Product product);
 
     public void DeleteProduct(int id);
