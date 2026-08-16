cf154ecf1973715702463b60274aef745b760879
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-16 22:46:20 +0530

    Updated file

--- src/API/Middleware/ExceptionMiddleware.cs
+++ src/API/Middleware/ExceptionMiddleware.cs
@@ -5,16 +5,13 @@ namespace InventoryManagementAPI.src.API.Middleware
 {
     public class ExceptionMiddleware
     {
-        private readonly RequestDelegate _next; 
+        private readonly RequestDelegate _next;
         private readonly ILogger<ExceptionMiddleware> _logger;
-
 
         public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
         {
             _next = next;
             _logger = logger;
-
-           
         }
 
         public async Task Invoke(HttpContext context)
